e80983cda3d4a5798153bcdb1529cc1f353441e1
Chloe Prime <chloe_von_einzbern@qq.com>
2025-07-22 22:32:15 +0800

    添加 GunScriptAPIExtension 可用的最小版本

--- src/main/java/mod/chloeprime/gunsmithlib/api/common/GunScriptAPIExtension.java
+++ src/main/java/mod/chloeprime/gunsmithlib/api/common/GunScriptAPIExtension.java
@@ -1,6 +1,12 @@
 package mod.chloeprime.gunsmithlib.api.common;
 
+/**
+ * @since 3.3.0
+ */
 @SuppressWarnings("unused")
 public interface GunScriptAPIExtension extends CommonScriptingExtension {
+    /**
+     * 播放 GunsmithLib 内置的过热音效
+     */
     void gunsmith_playOverheatSound();
 }
