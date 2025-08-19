7a08a26a576dfb68528034ca4cc0cadd0657cd71
Chloe Prime <chloe_von_einzbern@qq.com>
2025-08-19 18:13:28 +0800

    修复射击伤害加数在霰弹枪上显示不正确的bug

--- src/main/java/mod/chloeprime/gunsmithlib/mixin/client/MixinClientGunTooltip.java
+++ src/main/java/mod/chloeprime/gunsmithlib/mixin/client/MixinClientGunTooltip.java
@@ -2,6 +2,7 @@ package mod.chloeprime.gunsmithlib.mixin.client;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.tacz.guns.client.tooltip.ClientGunTooltip;
+import com.tacz.guns.resource.index.CommonGunIndex;
 import mod.chloeprime.gunsmithlib.api.common.GunAttributes;
 import mod.chloeprime.gunsmithlib.common.util.GsHelper;
 import net.minecraft.world.item.ItemStack;
@@ -12,15 +13,19 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(value = ClientGunTooltip.class, remap = false)
 public class MixinClientGunTooltip {
-    @Shadow
-    @Final
-    private ItemStack gun;
-
     @ModifyExpressionValue(
             method = "getText",
             at = @At(value = "INVOKE", target = "Lcom/tacz/guns/util/AttachmentDataUtils;getDamageWithAttachment(Lnet/minecraft/world/item/ItemStack;Lcom/tacz/guns/resource/pojo/data/gun/GunData;)D"))
     private double makeDisplayedDamageConsiderAttributeModifiers(double original) {
-        var item = this.gun;
-        return gun != null ? GsHelper.evaluateItemAttribute(item, GunAttributes.BULLET_DAMAGE, original) : original;
+        var gun = this.gun;
+        var gunIndex = this.gunIndex;
+        if (gun == null || gunIndex == null) {
+            return original;
+        }
+        var shrapnel = gunIndex.getBulletData().getBulletAmount();
+        return GsHelper.evaluateItemAttribute(gun, GunAttributes.BULLET_DAMAGE, original / shrapnel) * shrapnel;
     }
+
+    @Shadow @Final private ItemStack gun;
+    @Shadow @Final private CommonGunIndex gunIndex;
 }
