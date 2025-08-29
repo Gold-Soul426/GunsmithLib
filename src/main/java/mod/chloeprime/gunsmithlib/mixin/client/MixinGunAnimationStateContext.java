d08af26ef2f3045a9999c49dd43623515690546f
Chloe Prime <chloe_von_einzbern@qq.com>
2025-08-30 03:28:46 +0800

    [1.20.1] 修复精妙背包供弹对M870的支持在客户端不正确的bug

--- src/main/java/mod/chloeprime/gunsmithlib/mixin/client/MixinGunAnimationStateContext.java
+++ src/main/java/mod/chloeprime/gunsmithlib/mixin/client/MixinGunAnimationStateContext.java
@@ -1,21 +1,32 @@
 package mod.chloeprime.gunsmithlib.mixin.client;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.tacz.guns.api.item.IGun;
 import com.tacz.guns.client.animation.statemachine.GunAnimationStateContext;
 import mod.chloeprime.gunsmithlib.client.AbstractGunAnimationStateContextExtension;
+import mod.chloeprime.gunsmithlib.common.compat.CapabilityBasedModCompat;
 import mod.chloeprime.gunsmithlib.common.util.GsHelper;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
 
 import java.util.Optional;
+import java.util.function.Function;
 
 @Mixin(value = GunAnimationStateContext.class, remap = false)
-public class MixinGunAnimationStateContext implements AbstractGunAnimationStateContextExtension {
+public abstract class MixinGunAnimationStateContext implements AbstractGunAnimationStateContextExtension {
     @Shadow private ItemStack currentGunItem;
     @Shadow private IGun iGun;
+
+    // 扩展 API
+
+    @Shadow
+    protected abstract <T> Optional<T> processCameraEntity(Function<Entity, T> processor);
 
     @Override
     public float gunsmith_getCooldownSeconds() {
@@ -60,5 +71,24 @@ public class MixinGunAnimationStateContext implements AbstractGunAnimationStateC
     @Override
     public Optional<LivingEntity> gunsmithlib$getShooter() {
         return Optional.ofNullable(Minecraft.getInstance().player);
+    }
+
+    // 镜喵背包供弹实现
+
+    @ModifyReturnValue(method = "hasAmmoToConsume", at = @At("TAIL"))
+    private boolean makeHasAmmoToConsumeConsiderContainerItems(boolean original) {
+        return original || processCameraEntity(this::gunsmithlib$makeHasAmmoToConsumeConsiderContainerItems0).orElse(false);
+    }
+
+    @Unique
+    private boolean gunsmithlib$makeHasAmmoToConsumeConsiderContainerItems0(Entity entity) {
+        if (!(entity instanceof LivingEntity user)) {
+            return false;
+        }
+        var gunItem = currentGunItem;
+        if (gunItem == null) {
+            return false;
+        }
+        return CapabilityBasedModCompat.hasAmmoToConsume(user, gunItem);
     }
 }
