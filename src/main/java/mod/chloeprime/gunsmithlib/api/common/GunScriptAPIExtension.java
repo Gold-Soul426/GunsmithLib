b1643c9de84a723337d21497b1081882a9525d6c
Chloe Prime <chloe_von_einzbern@qq.com>
2026-03-11 10:34:21 +0800

    triggerAnimationStateTransition API

--- src/main/java/mod/chloeprime/gunsmithlib/api/common/GunScriptAPIExtension.java
+++ src/main/java/mod/chloeprime/gunsmithlib/api/common/GunScriptAPIExtension.java
@@ -9,4 +9,12 @@ public interface GunScriptAPIExtension extends CommonScriptingExtension {
      * 播放 GunsmithLib 内置的过热音效
      */
     void gunsmith_playOverheatSound();
+
+    /**
+     * 通知客户端触发状态机脚本状态转移（transition）
+     *
+     * @param input 状态机 transition 函数中的 input 参数
+     * @since 5.4.0
+     */
+    void gunsmith_triggerAnimationStateTransition(String input);
 }
--- src/main/java/mod/chloeprime/gunsmithlib/common/AbstractGunScriptAPIExtension.java
+++ src/main/java/mod/chloeprime/gunsmithlib/common/AbstractGunScriptAPIExtension.java
@@ -1,8 +1,25 @@
 package mod.chloeprime.gunsmithlib.common;
 
+import cn.chloeprime.commons.rpc.RPC;
+import cn.chloeprime.commons.rpc.RPCFlow;
+import cn.chloeprime.commons.rpc.RPCTarget;
+import cn.chloeprime.commons.rpc.RemoteCallable;
 import mod.chloeprime.gunsmithlib.api.common.GunScriptAPIExtension;
+import mod.chloeprime.gunsmithlib.client.GunsmithLibClient;
+import net.minecraft.server.level.ServerPlayer;
 import org.jetbrains.annotations.ApiStatus;
 
 @ApiStatus.Internal
 public interface AbstractGunScriptAPIExtension extends AbstractCommonScriptingExtension, GunScriptAPIExtension {
+    @Override
+    default void gunsmith_triggerAnimationStateTransition(String input) {
+        if (gunsmithlib$getShooter().orElse(null) instanceof ServerPlayer ssp) {
+            RPC.call(RPCTarget.to(ssp), AbstractGunScriptAPIExtension::triggerAnimation, input);
+        }
+    }
+
+    @RemoteCallable(flow = RPCFlow.SERVER_TO_CLIENT)
+    private static void triggerAnimation(String input) {
+        GunsmithLibClient.triggerAnimation(input);
+    }
 }
